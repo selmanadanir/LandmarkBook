e0fc09eb1e2a3ead5c42e08f4f0044407b1123fb
selman adanir <adanirselman@gmail.com>
2022-07-11 08:18:32 +0300

    Create xcode peoject

---
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Selman ADANİR on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
