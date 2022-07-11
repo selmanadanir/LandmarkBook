cb4046689096ba6963eea547d49f1accb739c8e8
selman adanir <adanirselman@gmail.com>
2022-07-11 11:14:51 +0300

    create tableview

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehange")
+        landmarkNames.append("Taj Mahal")
+        
+        
+        landmarkImages.append(UIImage(named: "colosseum.jpeg")!)
+        landmarkImages.append(UIImage(named: "great-wall.jpeg")!)
+        landmarkImages.append(UIImage(named: "kremlin.jpeg")!)
+        landmarkImages.append(UIImage(named: "stonehange.jpeg")!)
+        landmarkImages.append(UIImage(named: "taj-mahal.jpeg")!)
     }
-
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
+    
 }
-
